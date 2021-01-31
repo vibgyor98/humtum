e10de724a00dc9dd21293c348da488901ba6c20f
vibgyor98 <www.sunny02@gmail.com>
2021-01-31 17:47:15 +0530

    connected mongodb database

--- server/index.js
+++ server/index.js
@@ -3,3 +3,26 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+//userDefine Middleware
+app.use('/posts', postRoutes);
+
+//inbuilt Middileware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//MongoDB URL 
+const CONNECTION_URL = 'mongodb+srv://vibgyor98:cutemonster123@humtum.ioldk.mongodb.net/Memories?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
